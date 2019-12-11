f0e7d9c31e59f8d5e08b1a4f12da35af8bd1ff61
legendwk <Viking.Nilsson8260@student.uu.se>
2019-12-11 10:05:46 +0100

    knapprna är med

---
+++ src/components/MenuButtons.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  <button :class="['MenuButtons', {'is-green' : currentCategory === category}]" v-on:click="select">{{ uiLabels[title] }} </button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'MenuButtons',
+  props: {
+    uiLabels: Object,
+    lang: String,
+    title: String,
+    category: Number,
+    currentCategory: Number
+  },
+data: function() { //Note that data is a function!
+  return {
+  }
+},
+ methods: {
+select() {
+    this.$emit("selected", this.category)
+  }
+}
+}
+</script>
+
+<style scoped>
+
+.MenuButtons{
+  width:100px;
+  height:100px;
+  border-radius: 12px;
+  padding: 14px 28px;
+  text-align: center;
+}
+
+.is-grey{
+  background: grey;
+  border-color: black;
+  color: black;
+}
+
+.is-green{
+  background: green;
+  font-weight: bold;
+  color: white;
+  border-color: white;
+}
+
+</style>
